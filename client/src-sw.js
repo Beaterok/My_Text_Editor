f08d0c0d5bd55b6d26ce90c272909a764724908c
Alex Loburev <48661813+Beaterok@users.noreply.github.com>
2023-03-07 23:17:34 -0500

    second try at fixing bugs

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,9 +1,9 @@
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
-const { registerRoute } = require('workbox-routing');
+const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
+const { registerRoute,setDefaultHandler, setCatchHandler } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
-const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+const { precacheAndRoute, matchPrecache } = require('workbox-precaching/precacheAndRoute');
 
 precacheAndRoute(self.__WB_MANIFEST);
 
@@ -26,5 +26,42 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
+const offlineStrategy = new CacheFirst();
+
+warmStrategyCache({ //creates data for user to use 
+  urls: ['/offline.html'],
+  strategy: offlineStrategy,
+});
+
+setDefaultHandler( new StaleWhileRevalidate());
+
+setCatchHandler( async ({ request }) => {
+switch( request.destination) {
+  case 'document':
+    return matchPrecache(offline.html);
+  
+  default:
+    return Response.error();
+
+}
+});
+
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  // Here we define the callback function that will filter the requests we want to cache (in this case, JS and CSS files)
+  ({ request }) => ['style', 'script', 'worker', 'manifest','serviceworker'].includes(request.destination),
+  new CacheFirst({
+    // Name of the cache storage.
+    cacheName: 'asset-cache',
+    plugins: [
+      // This plugin will cache responses with these headers to a maximum-age of 30 days
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxAgeSeconds: 30 * 24 * 60 * 60,
+      }),
+    ],
+  })
+);
+
